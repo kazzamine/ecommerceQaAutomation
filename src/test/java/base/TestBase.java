e20447934ef55df27bcc9f2d691c043782340833
amine <amine.kazzout@mobiblanc.com>
2025-08-29 12:25:06 +0100

    project setup

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,39 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class TestBase {
+    protected WebDriver driver;
+    protected String baseUrl="https://automationexercise.com/";
+
+    @BeforeMethod
+    public void setup(){
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        options.addArguments("--disable-notifications", "--disable-infobars");
+        driver=new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        if(driver!=null){
+            driver.quit();
+
+        }
+    }
+
+
+}
